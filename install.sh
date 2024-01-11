ef7420adb5a2a9641d60bd215eb6cccb0ed96368
Biswajit Shaw <78879232+thegoddo@users.noreply.github.com>
2024-01-11 12:21:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ if [[ -f "$LOCAL_BIN" ]]; then
   rm -rf $LOCAL_BIN/apod_wallpaper/
 else
   mkdir -p $LOCAL_BIN
+  git clone https://github.com/thegoddo/apod_wallpaper.git $LOCAL_BIN
+  mv $LOCAL_BIN/apod_wallpaper/apodw ../
+  chmod u+x $LOCAL_BIN/apodw
+  rm -rf $LOCAL_BIN/apod_wallpaper/
 fi
 
 
